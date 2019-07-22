d84f83fd295960c195ce5b78bd7644f2f3e5f1a0
Marina Romanova <mrnromanova@gmail.com>
2019-07-22 13:33:09 +0200

    updated app.js to test feature branch

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Testing feature branches
+          Testing feature branches with Travis CI and Docker
         </p>
         <a
           className="App-link"
