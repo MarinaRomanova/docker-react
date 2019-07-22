1cf8a5eda79a50da8a3be4f824581aae6174e504
Marina Romanova <mrnromanova@gmail.com>
2019-07-22 13:31:36 +0200

    updated app.js to test feature branch

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          First signs of life registered
+          Testing feature branches
         </p>
         <a
           className="App-link"
